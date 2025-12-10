2670fa1a530d18fa000d278551a92d864f24b9dc
Vincent_Liussyaputra <liusvinv@gmail.com>
2025-12-10 15:04:27 +0700

    Add category and cls

--- schemaTypes/general.ts
+++ schemaTypes/general.ts
@@ -4,7 +4,32 @@ export default defineType({
   name: 'general',
   title: 'General',
   type: 'document',
-
+  groups: [
+    {
+      title: 'Global Settings',
+      name: 'global',
+    },
+    {
+      name: 'our_mission',
+      title: 'Our Mission Section',
+    },
+    {
+      name: 'about_us',
+      title: 'About Us Section',
+    },
+    {
+      name: 'testimonials',
+      title: 'Testimonials',
+    },
+    {
+      name: 'values',
+      title: 'Values',
+    },
+    {
+      name: 'cls',
+      title: 'Currently Livestreaming',
+    },
+  ],
   fields: [
     // Define your fields here
     defineField({
@@ -12,15 +37,18 @@ export default defineType({
       title: 'Preset ID',
       description: `set it as 'main' to use this preset`,
       type: 'string',
+      group: 'global',
     }),
     defineField({
       name: 'gated',
       title: 'Is Under Construction?',
+      group: 'global',
       type: 'boolean',
     }),
     defineField({
       name: 'gated_pass',
       title: 'Website Password',
+      group: 'global',
       type: 'string',
     }),
     defineField({
@@ -36,6 +64,7 @@ export default defineType({
       title: 'Categories Media',
       description: 'Categories section on main page, match the name with page slug',
       type: 'array',
+      group: 'global',
       of: [
         {
           type: 'object',
@@ -61,6 +90,7 @@ export default defineType({
     defineField({
       name: 'testimonials',
       type: 'array',
+      group: 'testimonials',
       of: [
         {
           type: 'object',
@@ -97,37 +127,57 @@ export default defineType({
     defineField({
       name: 'abi',
       title: 'About Us Image',
+      group: 'about_us',
       type: 'image',
     }),
     defineField({
       name: 'om',
       title: 'Our Mission Image',
       type: 'image',
+      group: 'our_mission',
     }),
     defineField({
       name: 'v1',
       title: 'Values 1',
       type: 'image',
+      group: 'values',
     }),
     defineField({
       name: 'v2',
+      group: 'values',
       title: 'Values 2',
       type: 'image',
     }),
     defineField({
       name: 'v3',
+      group: 'values',
       title: 'Values 3',
       type: 'image',
     }),
     defineField({
       name: 'omdesc',
       title: 'Our Mission Text',
+      group: 'our_mission',
       type: 'text',
     }),
     defineField({
       name: 'audesc',
       title: 'About Us Text',
+      group: 'about_us',
       type: 'text',
+    }),
+
+    defineField({
+      name: 'currentlyLive',
+      title: 'Currently Live Streaming',
+      group: 'cls',
+      type: 'array',
+      of: [
+        {
+          type: 'reference',
+          to: {type: 'creator'},
+        },
+      ],
     }),
   ],
 })
